63e6f3f7caa45a36f61d07c4f0e5f870f02183b2
Steven <wusteven815@gmail.com>
2023-01-28 13:54:02 -0500

    Add blank settings page

---
+++ app/screens/Settings/SettingsPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+
+function SettingsPage(props) {
+
+    return (
+
+        <View>
+            <Text>settings</Text>
+        </View>
+
+    );
+
+}
+
+
+export default SettingsPage;
