42cee66321c5c01d0cbcb9c25e5ee13d07dca7bf
Steven <wusteven815@gmail.com>
2023-01-31 01:07:59 -0500

    Start settings page

--- app/pages/Settings/SettingsPage.js
+++ app/pages/Settings/SettingsPage.js
@@ -1,19 +1,70 @@
-import React from "react";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import React, { useContext } from "react";
+import { Pressable, ScrollView, StyleSheet, View } from "react-native";
+import { MD3DarkTheme, Text } from "react-native-paper";
+import Category from "../../components/Category";
+import { Settings } from "../../settings";
+import { Default, Violet } from "../../config/themes";
 
 
 function SettingsPage() {
 
+    const settings = useContext(Settings);
+    const themes = [[Default, "Default"], [Violet, "Violet"]]
+
+    const themeControl = themes.map(theme => {
+
+        const name = theme[1];
+        theme = theme[0];
+        const outline = theme == settings.theme.colors ? theme.primary : Default.outlineVariant;
+
+        return (
+            <View key={"theme " + name} >
+                <Pressable
+                    style={[styles.themeCard, { backgroundColor: theme.background, borderColor: outline }]}
+                    onPress={() => settings.setTheme({ ...MD3DarkTheme, colors: theme })}
+                    key={"pressable " + name}>
+                    <View style={[styles.primaryCircle, { backgroundColor: theme.primary }]} key={"circle " + name} />
+                </Pressable>
+                <View style={{ width: 100, alignItems: "center", marginTop: 5 }} >
+                    <Text>{name}</Text>
+                </View>
+            </View>
+        );
+
+    });
+
     return (
 
         <View>
-            <Text>settings</Text>
+            <Category title="Appearance">
+                <Text variant="bodyLarge" style={{marginTop: 15}}>Theme</Text>
+                <ScrollView style={{ marginVertical: 10 }} horizontal={true}>
+                    {themeControl}
+                </ScrollView>
+            </Category>
         </View>
 
     );
 
 }
+
+const styles = StyleSheet.create({
+    primaryCircle: {
+        borderRadius: 12,
+        height: 24,
+        marginBottom: 20,
+        marginLeft: 15,
+        width: 24,
+    },
+    themeCard: {
+        borderRadius: 20,
+        borderWidth: 5,
+        height: 150,
+        justifyContent: "flex-end",
+        marginRight: 10,
+        width: 100,
+    }
+})
 
 
 export default SettingsPage;
