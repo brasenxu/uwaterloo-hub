7d4e9bc099a6619a3ae84b897c56c397c5659954
Steven <wusteven815@gmail.com>
2023-01-31 01:09:22 -0500

    Use Material Theme

--- app/components/MenuItem.js
+++ app/components/MenuItem.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Avatar, Text, TouchableRipple } from "react-native-paper";
 import { StyleSheet, View } from "react-native";
 
 import colors from "../config/colors";
+import { Settings } from "../settings";
 
 
 function MenuItem({ title, subtitle, icon, onPress }) {
 
+    const settings = useContext(Settings);
+
     return (
         <TouchableRipple onPress={onPress} rippleColor="#ffffff40">
             <View style={styles.item}>
-                <Avatar.Icon size={50} icon={icon} color={colors.primary} style={styles.iconAvatar} />
+                <Avatar.Icon size={50} icon={icon} color={settings.theme.colors.primary} style={styles.iconAvatar} />
                 <View style={styles.textContainer}>
                     <Text variant="titleMedium">{title}</Text>
                     {subtitle ? <Text variant="bodySmall">{subtitle}</Text> : undefined}
